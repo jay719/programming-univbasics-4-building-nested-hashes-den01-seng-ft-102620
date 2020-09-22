27f7a157a1fc04be4a9480f6dbd8deb46d5f5a7d
Javaria Brascom <javariab17@gmail.com>
2020-09-22 04:28:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,7 +45,7 @@ def monopoly_with_fourth_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 
-  :railroads=>
+ { :railroads=>
   {
     :pieces=>4,
     :rent_in_dollars=>
